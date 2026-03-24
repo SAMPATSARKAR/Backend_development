d25d3f24891f4b42fb5304d4d026013fb353d664
SAMPATSARKAR <sampat7123@gmail.com>
2026-03-25 00:32:10 +0530

    Vue.js

--- Development/Frontend/src/App.jsx
+++ Development/Frontend/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes,setJokes] = useState([])
+  useEffect(()=>{
+    axios.get('/api/jokes')
+    .then((res)=>{
+      setJokes(res.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  },[])
 
   return (
-    
+    <>
     <h1>Hello Sampat</h1>
+    <p>jokes: {jokes.length}</p>
+    {
+      jokes.map((joke,index)=>(
+        <div key={joke.id}>
+        <h3>{joke.setup}</h3>
+        <p>{joke.punchline}</p>
+        </div>
+      ))
+    }
+    </>
   )
 }
 
