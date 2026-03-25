874174e8caf2f39bdc083766b5846e7eb8a9c236
SAMPATSARKAR <sampat7123@gmail.com>
2026-03-26 00:08:35 +0530

    initial commit

---
+++ Development/Readme.md
@@ -0,0 +1,2 @@
+#backend Series
+MERN
---
+++ initial_Learning/index.js
@@ -0,0 +1,22 @@
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT|| 4000
+
+// app.get('/', (req, res) => {
+//   res.json({
+//     "name":"sampat"
+//   }) 
+// })
+// app.get('/twitter',(req,res)=>{
+//     res.send("sampat@gmail.com")
+// })
+// app.get('/login',(req,res)=>{
+//     res.send("<h1>please login at page<h1>")
+// })
+// app.get('/youtube',(req,res)=>{
+//     res.send("<h1>looking at youtube<h1>")
+// })
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
